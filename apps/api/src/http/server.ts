3942b2f906972f38fd85581a12daef54fbf1a8d7
lucas <lucas.pfranca01@gmail.com>
2024-12-30 11:33:59 -0300

    feat: routes to organizations and projects

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,14 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
+import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
+import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
+import { deleteeProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -65,6 +73,15 @@ app.register(requestPasswordRecover)
 app.register(resetPassword)
 
 app.register(createOrganization)
+app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
+app.register(updateOrganization)
+app.register(shutdownOrganization)
+app.register(transferOrganization)
+
+app.register(createProject)
+app.register(deleteeProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server runing!!')
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -17,6 +17,7 @@ import { userSubject } from './subjects/user'
 export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
+export * from './roles'
 
 const appAbilitiesSchema = z.union([
   projectSubject,
@@ -48,5 +49,7 @@ export function defineAbilityFor(user: User) {
     },
   })
 
+  ability.can = ability.can.bind(ability)
+  ability.cannot = ability.cannot.bind(ability)
   return ability
 }
